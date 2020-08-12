0b5617c50b7e4d9954112a99242b11c60271e406
eyad99 <iead.sh123@gmail.com>
2020-08-12 21:34:51 +0300

    modify file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                background-color: rgb(147, 39, 39);
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
